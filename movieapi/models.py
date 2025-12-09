d3028af80a748d050fc3a808171e87ce145b34de
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2025-12-09 22:50:25 +0000

    Add models for movies with fields. Make migrations and check in the user panel.

--- movieapi/models.py
+++ movieapi/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Movie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    tmdb_id = models.IntegerField()
+    image_path = models.CharField(max_length=255)
+    watched = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_on']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'tmdb_id'],
+                name='unique_user_tmdb_movie'
+            )
+        ]
+        
+    def __str__(self):
+        return self.title
