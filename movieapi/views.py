c58fb42287bc25d8c2b19e7c735b1d6977c4b233
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2025-12-14 16:59:56 +0000

    Add user registration view and urls to create a new user. Auto generate auth tokens in the response for auto log in.

--- movieapi/views.py
+++ movieapi/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import permissions, status
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer
 
-# Create your views here.
+
+
+class RegisterUserView(CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response(
+            {
+                "user": user.username,
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            },
+            status=status.HTTP_201_CREATED
+        )
+
