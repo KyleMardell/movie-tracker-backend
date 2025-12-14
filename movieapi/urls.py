458359eb931c5880978b0ce7a29d32c211445fd7
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2025-12-14 21:23:38 +0000

    Add views for movies, list create, delete and toggle watched status. Add urls for movie end points.

--- movieapi/urls.py
+++ movieapi/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import RegisterUserView
+from .views import RegisterUserView, MovieListCreateView, MovieDeleteView, MovieToggleWatchedView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('movies/', MovieListCreateView.as_view(), name='movie-list-create'),
+    path('movies/<int:pk>/', MovieDeleteView.as_view(), name='movie-delete'),
+    path('movies/<int:pk>/toggle-watched/', MovieToggleWatchedView.as_view(), name='movie-toggle-watched'),
 ]
--- movieapi/views.py
+++ movieapi/views.py
@@ -1,10 +1,11 @@
 from rest_framework import permissions, status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, DestroyAPIView, UpdateAPIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+from .models import Movie
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import RegisterSerializer
-
+from .serializers import RegisterSerializer, MovieSerializer
 
 
 class RegisterUserView(CreateAPIView):
@@ -26,3 +27,44 @@ class RegisterUserView(CreateAPIView):
             status=status.HTTP_201_CREATED
         )
 
+
+class MovieListCreateView(ListCreateAPIView):
+    
+    serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(user=self.request.user)
+        watched_param = self.request.query_params.get('watched')
+        if watched_param is not None:
+            if watched_param.lower() == 'true':
+                queryset = queryset.filter(watched=True)
+            elif watched_param.lower() == 'false':
+                queryset = queryset.filter(watched=False)
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+    
+class MovieDeleteView(DestroyAPIView):
+    serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Movie.objects.filter(user=self.request.user)
+    
+    
+class MovieToggleWatchedView(UpdateAPIView):
+    serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Movie.objects.filter(user=self.request.user)
+
+    def get_object(self):
+        obj = super().get_object()
+        # toggle the watched status
+        obj.watched = not obj.watched
+        obj.save()
+        return obj
