2792316809091d1e86000b555859bc879ca5c279
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2025-12-15 18:58:05 +0000

    Add comment to models and change dockerfile name for deployment.

--- movieapi/models.py
+++ movieapi/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+""" 
+    Movie Model
+    contains fields for user, title, tmdb id, image and watched status
+"""
 class Movie(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
