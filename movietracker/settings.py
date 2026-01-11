cac135f20e68a4355d4f0183d605bfd61119eeb7
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2026-01-11 15:09:28 +0000

    set pagination to none.

--- movietracker/settings.py
+++ movietracker/settings.py
@@ -145,6 +145,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': None,
 }
 
 # JWT TOKEN DURATION
